39ba8be0116ebe4201671d14cf08f6f377aabaea
Axel Vallon <axel.vallon123@gmail.com>
2021-11-05 21:36:20 +0100

    add cart.jsp to show cart

--- src/main/java/com/amt/cart/CartServlet.java
+++ src/main/java/com/amt/cart/CartServlet.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO create dispacher showing cart. Note : 2 cases, member and visitor
+        RequestDispatcher rd = request.getRequestDispatcher("cart.jsp");
+        rd.forward(request, response);
     }
 
     @Override
