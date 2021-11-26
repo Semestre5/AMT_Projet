a28f37aea70dd435ea6dad22a72a80dbdc632a28
Axel Vallon <axel.vallon123@gmail.com>
2021-11-26 18:09:06 +0100

    youpi

--- src/main/java/com/amt/shopManagement/ArticleAddServlet.java
+++ src/main/java/com/amt/shopManagement/ArticleAddServlet.java
@@ -6,15 +6,16 @@ import com.DAO.Objects.Article;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 @WebServlet(name = "ArticleAddServlet", value = "/articleAdd")
 @MultipartConfig(
         fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
         maxFileSize = 1024 * 1024 * 10,      // 10 MB
-        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+        maxRequestSize = 1024 * 1024 * 10  // 10 MB
 )
 public class ArticleAddServlet extends HttpServlet {
     private static final String BASE_IMG_URL = "https://media.istockphoto.com/vectors/yellow-warning-sign-work-in-progress-background-vector-id1253437873?k=20&m=1253437873&s=612x612&w=0&h=S0hzCYCgPCufkdZaHlCDTCu_LP066lD99njJm8RuPgg=";
@@ -33,26 +34,14 @@ public class ArticleAddServlet extends HttpServlet {
         String imageLink = request.getParameter("image");
         String name = request.getParameter("name");
         Integer quantity = Integer.valueOf(request.getParameter("quantity"));
-
-        Part filePart = request.getPart("image");
-        if (filePart != null) {
-            String fileName = filePart.getSubmittedFileName();
-            String uploadRelativePath = "\\resources\\images\\";
-            for (Part part : request.getParts()) {
-                String uploadPath = getServletContext().getRealPath("") + "\\..\\..\\src\\main\\webapp" + uploadRelativePath + fileName;
-                part.write(uploadPath);
-            }
-            imageLink = "./resources/images/" + fileName;
-        } else {
-            imageLink = BASE_IMG_URL;
-        }
-        if(description == ""){
+        uploadImage(request.getPart("image"));
+        if(Objects.equals(description, "")){
             description = "The seller hasn't implemented a description yet";
         }
-        if(priceInput != ""){
+        /*if(!Objects.equals(priceInput, "")){
             price =  BigDecimal.valueOf(Long.parseLong(priceInput));
-        }
-        Article newArticle = new Article(price,description,name,quantity, imageLink);
+        }*/
+        Article newArticle = new Article(BigDecimal.valueOf(1),description,name,quantity, imageLink);
         List<Article> currentArticles = (List<Article>) ArticleOps.fetchAll();
         boolean already_exists = false;
         Article duplicate = null;
@@ -72,4 +61,27 @@ public class ArticleAddServlet extends HttpServlet {
             rd.forward(request, response);
         }
     }
+
+    private void uploadImage(Part filePart) throws IOException {
+        if (filePart != null) {
+            String fileName = getFileName(filePart);
+            File uploadDir = new File( getServletContext().getRealPath("/images") );
+            if ( ! uploadDir.exists() )
+                uploadDir.mkdir();
+            String fullPath = getServletContext().getRealPath("/images") + File.separator + fileName;
+            filePart.write(fullPath);
+        }
+    }
+
+    private String getFileName(final Part part) {
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+
 }
